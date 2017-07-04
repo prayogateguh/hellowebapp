a6faf9ac5c3a2860276651ae7e0b3cc6593bbad5
Prayoga Teguh <teguh.p@students.amikom.ac.id>
2017-07-04 07:52:46 +0700

    edit post/things using django modelform

--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from collection.forms import ThingForm 
 from collection.models import Thing
 
 def index(request):
@@ -14,3 +15,23 @@ def thing_detail(request, slug):
 
     # pass to the template
     return render(request, 'things/thing_detail.html', {'thing': thing,})
+
+def edit_thing(request, slug):
+    thing = Thing.objects.get(slug=slug)
+
+    form_class = ThingForm
+
+    # if we're coming to this view from a submitted form
+    if request.method == 'POST':
+        # grab the data from the sumbitted form and apply to the form
+        form = form_class(data=request.POST, instance=thing)
+        if form.is_valid():
+            # save the new data
+            form.save()
+            return redirect('thing_detail', slug=thing.slug)
+    # otherwise just create the form
+    else:
+        form = form_class(instance=thing)
+    
+    # and render the template
+    return render(request, 'things/edit_thing.html', {'thing': thing, 'form': form,})
